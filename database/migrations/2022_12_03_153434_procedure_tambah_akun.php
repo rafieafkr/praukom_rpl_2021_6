7223ad60ce22464b80442f1b80fd6dfa11357805
unknown <rafie.afkar@gmail.com>
2022-12-07 10:13:00 +0700

    fix procedure bug

--- database/migrations/2022_12_03_153434_procedure_tambah_akun.php
+++ database/migrations/2022_12_03_153434_procedure_tambah_akun.php
@@ -15,32 +15,32 @@ return new class extends Migration
     public function up()
     {
         DB::unprepared('
-        CREATE OR REPLACE PROCEDURE procedure_tambah_akun (Level_user INT, Email VARCHAR(60), Password VARCHAR(60), Username VARCHAR(60), Nama VARCHAR(60), Identitas VARCHAR(20))
+        CREATE OR REPLACE PROCEDURE procedure_tambah_akun (nLevel_user INT, nEmail VARCHAR(60), nPassword VARCHAR(60), nUsername VARCHAR(60), nNama VARCHAR(60), nIdentitas VARCHAR(20))
           BEGIN
             DECLARE idakun INT;
             DECLARE idguru INT;
-            INSERT INTO akun (level_user,email,password,username) VALUES (Level_user, Email, Password, Username);
-            SELECT id_akun INTO idakun FROM akun WHERE username=Username LIMIT 1;
-            IF(Level_user=1) THEN
-              INSERT INTO guru (id_akun,nip_guru,nama_guru) VALUES (idakun, Identitas, Nama);
-              SELECT id_guru INTO idguru FROM guru WHERE id_akun=idakun LIMIT 1;
+            INSERT INTO akun (level_user,email,password,username) VALUES (nLevel_user, nEmail, nPassword, nUsername);
+            SELECT id_akun INTO idakun FROM akun WHERE username=nUsername;
+            IF(nLevel_user=1) THEN
+              INSERT INTO guru (id_akun,nip_guru,nama_guru) VALUES (idakun, nIdentitas, nNama);
+              SELECT id_guru INTO idguru FROM guru WHERE id_akun=idakun;
               INSERT INTO staff_hubin (id_guru) VALUES (idguru);
-            ELSEIF(Level_user=2) THEN
-              INSERT INTO guru (id_akun,nip_guru,nama_guru) VALUES (idakun, Identitas, Nama);
-              SELECT id_guru INTO idguru FROM guru WHERE id_akun=idakun LIMIT 1;
+            ELSEIF(nLevel_user=2) THEN
+              INSERT INTO guru (id_akun,nip_guru,nama_guru) VALUES (idakun, nIdentitas, nNama);
+              SELECT id_guru INTO idguru FROM guru WHERE id_akun=idakun;
               INSERT INTO kepala_program (id_guru) VALUES (idguru);
-            ELSEIF(Level_user=3) THEN
-              INSERT INTO guru (id_akun,nip_guru,nama_guru) VALUES (idakun, Identitas, Nama);
-              SELECT id_guru INTO idguru FROM guru WHERE id_akun=idakun LIMIT 1;
+            ELSEIF(nLevel_user=3) THEN
+              INSERT INTO guru (id_akun,nip_guru,nama_guru) VALUES (idakun, nIdentitas, nNama);
+              SELECT id_guru INTO idguru FROM guru WHERE id_akun=idakun;
               INSERT INTO wali_kelas (id_guru) VALUES (idguru);
-            ELSEIF(Level_user=4) THEN
-              INSERT INTO guru (id_akun,nip_guru,nama_guru) VALUES (idakun, Identitas, Nama);
-              SELECT id_guru INTO idguru FROM guru WHERE id_akun=idakun LIMIT 1;
+            ELSEIF(nLevel_user=4) THEN
+              INSERT INTO guru (id_akun,nip_guru,nama_guru) VALUES (idakun, nIdentitas, nNama);
+              SELECT id_guru INTO idguru FROM guru WHERE id_akun=idakun;
               INSERT INTO pembimbing_sekolah (id_guru) VALUES (idguru);
-            ELSEIF(Level_user=5) THEN
-              INSERT INTO pembimbing_perusahaan (nik_pp,id_akun,nama_pp) VALUES (Identitas,idakun,Nama);
+            ELSEIF(nLevel_user=5) THEN
+              INSERT INTO pembimbing_perusahaan (nik_pp,id_akun,nama_pp) VALUES (nIdentitas,idakun,nNama);
             ELSE
-              INSERT INTO siswa (nis,id_akun,nama_siswa) VALUES (Identitas,idakun,Nama);
+              INSERT INTO siswa (nis,id_akun,nama_siswa) VALUES (nIdentitas,idakun,nNama);
             END IF;
           END;
         ');
