66cf8f17dce667e10a1183264a682c4f64e250b8
Mr-Nrb <114650137+Mr-Nrb@users.noreply.github.com>
2023-01-19 13:56:15 +0700

    Add files via upload

---
+++ database/migrations/2023_01_19_024621_view_penilaian.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("
+        CREATE VIEW view_penilaian  AS 
+        SELECT 
+                siswa.nama_siswa, 
+                siswa.nis, 
+                pembimbing_perusahaan.nama_pp, 
+                penilaian.id_penilaian,
+                nilai.nilai,
+                kompetensi.nama_kompetensi
+
+        FROM penilaian
+        
+        INNER JOIN siswa ON penilaian.nis = siswa.nis
+        INNER JOIN nilai ON penilaian.id_penilaian = nilai.id_penilaian
+        INNER JOIN pembimbing_perusahaan ON penilaian.nik_pp = pembimbing_perusahaan.nik_pp
+        INNER JOIN kompetensi ON nilai.kompetensi = kompetensi.id_kompetensi
+
+    ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
