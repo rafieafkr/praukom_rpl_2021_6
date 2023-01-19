288ee813ef7596bac72c5bed6c7f4f903298a656
Mr-Nrb <114650137+Mr-Nrb@users.noreply.github.com>
2023-01-19 13:57:53 +0700

    Add files via upload

---
+++ database/migrations/2022_12_01_010341_trigger_insert_penilaian_from_prakerin.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("
+            CREATE TRIGGER insertnilai_dari_prakerin 
+            AFTER INSERT ON prakerin 
+            FOR EACH ROW 
+            BEGIN 
+            INSERT INTO penilaian VALUES (
+                 '',
+             NEW.nis,
+             NEW.nik_pp
+            );
+            END
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('prakerin', function (Blueprint $table) {
+            //
+        });
+    }
+};
