4417d75cafe511b7a71020d7d753af084b929142
Mr-Nrb <114650137+Mr-Nrb@users.noreply.github.com>
2023-01-19 09:32:33 +0700

    Add files via upload

---
+++ database/migrations/2023_01_18_024924_view_presensi.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        DB::unprepared("
+        CREATE VIEW view_presensi AS 
+        SELECT 
+                siswa.nama_siswa, 
+                siswa.nis, 
+                pembimbing_perusahaan.nama_pp, 
+                presensi_siswa.tgl_kehadiran, 
+                presensi_siswa.jam_masuk,
+                presensi_siswa.jam_keluar,
+                presensi_siswa.keterangan,
+                presensi_siswa.kegiatan,
+                presensi_siswa.status,
+                presensi_siswa.id_presensi
+
+        FROM presensi_siswa
+        
+        INNER JOIN siswa ON presensi_siswa.nis = siswa.nis
+        INNER JOIN pembimbing_perusahaan ON presensi_siswa.nik_pp = pembimbing_perusahaan.nik_pp
+
+    ");
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
