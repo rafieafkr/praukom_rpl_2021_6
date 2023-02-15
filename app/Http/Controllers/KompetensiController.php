bfbe03c481d152c0a82eb1a25be5fff0ce57c726
Mr-Nrb <114650137+Mr-Nrb@users.noreply.github.com>
2023-02-15 10:19:50 +0700

    Update KompetensiController.php

--- app/Http/Controllers/KompetensiController.php
+++ app/Http/Controllers/KompetensiController.php
@@ -3,84 +3,116 @@
 namespace App\Http\Controllers;
 
 use App\Models\Kompetensi;
-use App\Http\Requests\StoreKompetensiRequest;
-use App\Http\Requests\UpdateKompetensiRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class KompetensiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    protected $kompetensi;
+    public function __construct()
+    {
+        $this->kompetensi = new Kompetensi();
+    }
     public function index()
     {
-        //
-    }
+        $dataview = DB::table('view_kompetensi')
+            ->select()
+            ->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $datajurusan = DB::table('view_jurusan_kompetensi')
+            ->select()
+            ->get();
+            // dd($datajurusan);
+        
+        return view('dashboard.modules.kompetensi',[
+                'dataview'=>$dataview,
+                'datajurusan'=>$datajurusan
+              ]);
     }
+    
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreKompetensiRequest  $request
+     * @param  \App\Http\Requests\StorePenilaianRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreKompetensiRequest $request)
+    public function simpan(Request $request)
     {
-        //
+        // dd($request);
+        //insert tabel nilai
+          Kompetensi::create([
+            "nama_kompetensi" => $request->nama_kompetensi,
+            "id_jurusan" => $request->id_jurusan,
+          ]);
+          return redirect('kompetensi');
+
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Kompetensi  $kompetensi
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Kompetensi $kompetensi)
+    public function hapus($id_kompetensi)
     {
-        //
+        
+        $hapus = DB::table('kompetensi')
+                        ->where('id_kompetensi',$id_kompetensi)
+                        ->delete();
+      
+        if($hapus){
+            return back();
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Kompetensi  $kompetensi
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Kompetensi $kompetensi)
-    {
-        //
-    }
+    public function cari(Request $request)
+	{
+        $dataview = DB::table('view_kompetensi')
+        ->select()
+        ->get();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateKompetensiRequest  $request
-     * @param  \App\Models\Kompetensi  $kompetensi
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateKompetensiRequest $request, Kompetensi $kompetensi)
-    {
-        //
-    }
+        $datajurusan = DB::table('jurusan')
+        ->select()
+        ->get();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Kompetensi  $kompetensi
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Kompetensi $kompetensi)
+		$cari = $request->cari;
+ 
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$dataview = DB::table('view_kompetensi')
+		->where('nama_kompetensi','like',"%".$cari."%")
+        ->orWhere('nama_jurusan','like',"%".$cari."%")
+		->paginate();
+ 
+    		// mengirim data pegawai ke view index
+		return view('dashboard.modules.kompetensi',['dataview' => $dataview,
+                                                    'datajurusan' => $datajurusan
+                                                    ]);
+ 
+	}
+  
+       //UPDATE 
+   public function edit($id)
+   {
+
+       $datakompetensi = DB::table('view_kompetensi')
+                    ->where('id_kompetensi', $id)
+                    ->select()
+                    ->get();
+                    // dd($datajurusan);
+
+       return view('dashboard.modules.editkompetensi', ['datakompetensi' => $datakompetensi]);
+   }
+   public function update(Request $request)
     {
-        //
-    }
+        // dd($request);
+      
+        DB::table('kompetensi')
+            ->where('id_kompetensi','=', $request->id_kompetensi)
+
+        ->update([
+        
+            'nama_kompetensi' => $request->nama_kompetensi
+        ]);
+
+        return redirect('kompetensi');
+           
+    } 
+
 }
