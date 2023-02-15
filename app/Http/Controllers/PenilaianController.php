a03d82d0218c0f72086ab3ed48b787aa3785789d
Mr-Nrb <114650137+Mr-Nrb@users.noreply.github.com>
2023-02-15 07:57:17 +0700

    Update PenilaianController.php

--- app/Http/Controllers/PenilaianController.php
+++ app/Http/Controllers/PenilaianController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
+use App\Http\Controllers\flash;
+use Illuminate\Support\Facades\Hash;
 use App\Models\Penilaian;
 use App\Models\Nilai;
-
+use App\Models\Sikap;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -18,16 +18,24 @@ class PenilaianController extends Controller
      */
     protected $penilaian;
     protected $nilai;
+    protected $sikap;
     public function __construct()
     {
         $this->penilaian = new Penilaian();
         $this->nilai = new Nilai();
+        $this->sikap = new Sikap();
     }
     public function index()
     {
         $jurusan = DB::table('jurusan')
             ->get();
-        
+
+        $datasikap = DB::table('sikap')
+        ->join('penilaian', 'penilaian.id_penilaian', '=', 'sikap.id_penilaian')
+        ->join('siswa', 'siswa.nis', '=', 'penilaian.nis')
+        ->select()
+        ->get();
+        // dd($datasikap);
         //get data view
         $dataview = DB::table('penilaian')
             ->join('pembimbing_perusahaan', 'pembimbing_perusahaan.nik_pp', '=', 'penilaian.nik_pp')
@@ -46,7 +54,8 @@ class PenilaianController extends Controller
             // dd($nilaisiswa);
             return view('dashboard.modules.penilaian',compact('jurusan'),[
               'nilaisiswa'=>$nilaisiswa,
-              'dataview'=>$dataview
+              'dataview'=>$dataview,
+              'datasikap'=>$datasikap
             ]);
 
     }
@@ -58,6 +67,7 @@ class PenilaianController extends Controller
      */
     public function getKompetensi(Request $request)
     {
+        // dd($request);
         $kompetensi = DB::table('kompetensi')
             ->where('id_jurusan', $request->id_jurusan)
             ->get();
@@ -73,7 +83,7 @@ class PenilaianController extends Controller
      * @param  \App\Http\Requests\StorePenilaianRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function simpan(Request $request)
+    public function simpanKompetensi(Request $request)
     {
         //insert tabel nilai
         try {
@@ -88,7 +98,25 @@ class PenilaianController extends Controller
         return back()->withSucces('Nilai tersimpan.');
     }
 
-    public function cari(Request $request)
+    public function simpanSikap(Request $request)
+    {
+        //insert tabel nilai
+        try {
+          Sikap::create([
+            "id_penilaian" => $request->id_penilaian,
+            "disiplin_waktu" => $request->disiplin_waktu,
+            "kemauan_kerja_dan_motivasi" => $request->kemauan_kerja_dan_motivasi,
+            "kualitas_kerja" => $request->kualitas_kerja,
+            "inisiatif_kerja" => $request->inisiatif_kerja,
+            "perilaku" => $request->perilaku
+          ]);
+        } catch (\Exception) {
+          return back()->withErrors('Input nilai gagal.');
+        }
+        return back()->withSucces('Nilai tersimpan.');
+    }
+
+    public function cariTableKompetensi(Request $request)
 	{
 		// menangkap data pencarian
         $prakerin = DB::table('prakerin')
@@ -97,29 +125,92 @@ class PenilaianController extends Controller
 
         $jurusan = DB::table('jurusan')
         ->get();
+
+
+        $datasikap = DB::table('sikap')
+        ->join('penilaian', 'penilaian.id_penilaian', '=', 'sikap.id_penilaian')
+        ->join('siswa', 'siswa.nis', '=', 'penilaian.nis')
+        ->select()
+        ->get();
+
+        $nilaisiswa = DB::table('view_penilaian')
+        ->select()
+        ->get();
+
+        if ($request->cari) {
+            $cari = $request->cari;
+     
+            // mengambil data dari table pegawai sesuai pencarian data
+            $dataview = DB::table('view_penilaian')
+            ->where('nama_siswa','like',"%".$cari."%")
+            ->orWhere('nis','like',"%".$cari."%")
+            ->orWhere('nama_kompetensi','like',"%".$cari."%")
+            ->orWhere('nama_pp','like',"%".$cari."%")
+            ->paginate();
+        } else {
+            $dataview = [];
+        }
+
+    		// mengirim data pegawai ke view index
+		return view('dashboard.modules.penilaian',['dataview' => $dataview,
+                                                'prakerin' => $prakerin,
+                                                'jurusan' => $jurusan,
+                                                'nilaisiswa' => $nilaisiswa,
+                                                'datasikap' => $datasikap]);
+ 
+	}
+
+    public function cariTableSikap(Request $request)
+	{
+		// menangkap data pencarian
+        $prakerin = DB::table('prakerin')
+        ->select()
+        ->get();
+
+        $dataview = DB::table('view_penilaian')
+        ->select()
+        ->get();
+
+        $jurusan = DB::table('jurusan')
+        ->get();
+
+        $datasikap = DB::table('sikap')
+        ->join('penilaian', 'penilaian.id_penilaian', '=', 'sikap.id_penilaian')
+        ->join('siswa', 'siswa.nis', '=', 'penilaian.nis')
+        ->select()
+        ->get();
     
 
         $nilaisiswa = DB::table('view_penilaian')
         ->select()
         ->get();
 
-		$cari = $request->cari;
- 
-    		// mengambil data dari table pegawai sesuai pencarian data
-		$dataview = DB::table('view_penilaian')
-		->where('nama_siswa','like',"%".$cari."%")
-		->paginate();
+		if ($request->cari2) {
+            $cari2 = $request->cari2;
+    
+            $datasikap = DB::table('view_sikap')
+            ->where('nama_siswa','like',"%".$cari2."%")
+            ->orWhere('nis','like',"%".$cari2."%")
+            ->paginate();
+        } else {
+            $datasikap = [];
+        }
  
     		// mengirim data pegawai ke view index
 		return view('dashboard.modules.penilaian',['dataview' => $dataview,
                                                 'prakerin' => $prakerin,
                                                 'jurusan' => $jurusan,
-                                                'nilaisiswa' => $nilaisiswa]);
+                                                'nilaisiswa' => $nilaisiswa,
+                                                'datasikap' => $datasikap
+                                               ]);
  
 	}
+
+
+    
   
        //UPDATE 
-   public function edit($id)
+   public function editKompetensi($id)
    {
        $datanilai = DB::table('nilai')
                     ->join('kompetensi', 'kompetensi.id_kompetensi', '=', 'nilai.kompetensi')
@@ -129,10 +220,19 @@ class PenilaianController extends Controller
 
        return view('dashboard.modules.editnilai', ['datanilai' => $datanilai]);
    }
-   public function update(Request $request)
+
+   public function editSikap($id)
+   {
+       $datasikap = DB::table('sikap')
+                    ->where('id_penilaian', $id, 'sikap.*')
+                    ->get();
+
+       return view('dashboard.modules.editsikap', ['datasikap' => $datasikap]);
+   }
+   public function updateKompetensi(Request $request)
     {
         // dd($request);
-      
+        
         DB::table('nilai')
             ->where('id_penilaian','=', $request->id_penilaian)
             ->where('kompetensi','=', $request->kompetensi)
@@ -141,25 +241,56 @@ class PenilaianController extends Controller
         
             // 'id_penilaian' => $request->id_penilaian,
             // 'kompetensi' => $request->input->kompetensi,
-            'nilai' => $request->nilai
+            'nilai' => $request->nilai,
 
         ]);
 
-        return back();
+
+        return redirect('penilaian');
+           
+    } 
+
+    public function updateSikap(Request $request)
+    {
+        // dd($request);
+
+        DB::table('sikap')
+        ->where('id_penilaian','=', $request->id_penilaian)
+        
+        ->update([
+    
+        // 'id_penilaian' => $request->id_penilaian,
+        "disiplin_waktu" => $request->disiplin_waktu,
+        "kemauan_kerja_dan_motivasi" => $request->kemauan_kerja_dan_motivasi,
+        "kualitas_kerja" => $request->kualitas_kerja,
+        "inisiatif_kerja" => $request->inisiatif_kerja,
+        "perilaku" => $request->perilaku
+
+    ]);
+
+        return redirect('penilaian');
            
     } 
     //METHOD PRINT NILAI
     public function print($id)
     {
-     
+                    
         $data_penilaian = DB::table('view_penilaian')
-                     ->where('nis', $id)
+                     ->where('id_penilaian', $id)
+                     ->select()
+                     ->get();
+
+        $data_sikap = DB::table('view_sikap')
+                     ->where('id_penilaian', $id)
                      ->select()
                      ->get();
                     //  dd($data_presensi);
  
-        return view('dashboard.modules.laporan_penilaian', ['data_penilaian' => $data_penilaian]);
+        return view('dashboard.modules.laporan_penilaian', ['data_penilaian' => $data_penilaian,
+                                                            'data_sikap' => $data_sikap]);
     }
+
+   
    }
     
    
