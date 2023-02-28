18bc4be595b65c04ecbfa72f2bc0c3520a454332
Mr-Nrb <114650137+Mr-Nrb@users.noreply.github.com>
2023-02-28 08:55:41 +0700

    Add files via upload

---
+++ resources/views/dashboard/modules/editangkatan.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Angkatan</title>
+    @vite('resources/css/app.css')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body class="bg-[#CCE0DD] h-screen">
+    <div class="navbar bg-base-100 shadow-lg">
+        <div class="navbar-start ">
+            <div class="dropdown">
+                <label tabindex="0" class="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h8m-8 6h16" />
+                    </svg>
+                </label>
+                <ul tabindex="0"
+                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+
+            </div>
+            <a class="btn btn-ghost normal-case text-xl">SIMAK</a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+
+        </div>
+        <div class="navbar-end ">
+            <a class="btn">LogOut</a>
+        </div>
+    </div>
+    <!-- Form Surat Pengajuan  -->
+    <div class="ml-5">
+        <a href="/kelas"><button class="btn">Kembali</button></a>
+    </div>
+
+    <div class="container px-5 mt-10 bg-white pb-5 overflow-auto w-full mx-auto">
+        <div class="bg-white pb-10 w-full">
+           <!-- Judul Form -->
+           <div class="w-full text-center mb-10 pt-5">
+                <p class="font-extrabold">Form Nilai Kompetensi Siswa</p>
+            </div>
+
+            @foreach($dataangkatan as $d)
+            <form action="/update/angkatan" method="POST">
+                <!-- {{csrf_field()}} -->
+                @csrf
+
+                <div class=" w-full px-2  font-bold">
+
+                    <div class="w-full">
+
+                        <input type="hidden" id="id_angkatan" name="id_angkatan" value="{{$d->id_angkatan}}" />
+                        <label for="">Tahun Angkatan</label>
+                        <input name='tahun' value="{{$d->tahun}}"
+                            class="rounded-md shadow-inner w-full border border-gray-400">
+
+                        <button type="submit" value="simpan"
+                            class="btn bg-[#256D85] rounded-lg px-5 mb-5 py-3 text-center shadow-md hover:bg-emerald-700 font-bold text-white">Simpan</button>
+                    </div>
+                </div>
+
+            </form>
+            @endforeach
+         
+        </div>
+    </div>
+</body>
+<script src="https://code.iconify.design/iconify-icon/1.0.1/iconify-icon.min.js"></script>
+
+</html>
